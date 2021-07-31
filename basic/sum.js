52cb443b90cfcecb0dd031c77982d33fcfa062c2
TaylorLiu11 <liuayo1111@gmail.com>
2021-07-31 12:27:27 +0800

    Create sum.js

---
+++ basic/sum.js
@@ -0,0 +1,17 @@
+console.log('Hello World');
+
+function sum(n) {
+    let result = 0;
+    result = (n + 1) * n / 2;
+
+    return result;
+}
+
+console.log(sum(1));
+console.log(sum(2));
+console.log(sum(3));
+console.log(sum(4));
+console.log(sum(5));
+console.log(sum(6));
+console.log(sum(10));
+console.log(sum(13));
