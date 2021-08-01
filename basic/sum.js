1d573c47e583229fd052c8f7f720915ac71151af
TaylorLiu11 <liuayo1111@gmail.com>
2021-08-01 09:43:21 +0800

    Update some note

--- basic/sum.js
+++ basic/sum.js
@@ -1,11 +1,26 @@
 console.log('Hello World');
 
+// My solution
+// function sum(n) {
+//     let result = 0;
+//     result = (n + 1) * n / 2;
+
+//     return result;
+// }
+
 function sum(n) {
-    let result = 0;
+    return (n + 1) * n / 2;
+}
+
+// Caution
+function sum(n) {
+    // If we don't specify variable this will still work, but it's dangerous!!
+    result = 0;
     result = (n + 1) * n / 2;
 
     return result;
 }
+
 
 console.log(sum(1));
 console.log(sum(2));
@@ -15,3 +30,4 @@ console.log(sum(5));
 console.log(sum(6));
 console.log(sum(10));
 console.log(sum(13));
+console.log(sum(18));
