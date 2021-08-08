7fae73ac4112ee99ae16923b860e0ed55225827b
TaylorLiu11 <liuayo1111@gmail.com>
2021-08-08 15:17:34 +0800

    Add .env

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -2,13 +2,14 @@ const axios = require('axios');
 const moment = require('moment');
 const fs = require("fs");
 const mysql = require("mysql");
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "admin",
-    port: 3306,
-    password: "12345",
-    database: "stock",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
