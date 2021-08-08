27cc9690539f5f9b71f31fa9d61bf800d569854d
TaylorLiu11 <liuayo1111@gmail.com>
2021-08-08 15:36:06 +0800

    Create crawler-db.js

--- crawler/crawler-db.js
+++ crawler/crawler-db.js
@@ -1,6 +1,25 @@
 const axios = require('axios');
 const moment = require('moment');
 const fs = require("fs");
+const mysql = require("mysql");
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error("資料庫連不上");
+    }
+});
+
+// 不關閉連線，認為程式一直在執行
+connection.end();
 
 (async function getStockName() {
     let stockCode = await new Promise((res, rej) => {
