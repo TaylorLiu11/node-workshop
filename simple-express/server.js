885e11468c9ba6427720f1568a72d39020613905
TaylorLiu11 <liuayo1111@gmail.com>
2021-08-15 11:27:37 +0800

    Create 2 middleware

--- simple-express/server.js
+++ simple-express/server.js
@@ -3,6 +3,16 @@ const express = require('express');
 // 利用 express 建立 express application
 let app = express();
 
+app.use((req, res, next) => {
+    console.log('Check 123');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log(req.method);
+    next();
+});
+
 app.get('/', (req, res, next) => {
     res.send('Hello');
 });
