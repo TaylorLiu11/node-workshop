f7e8e4f210f07c7c7eaab9077fb81bec8086092d
TaylorLiu11 <liuayo1111@gmail.com>
2021-08-24 18:14:32 +0800

    作業1、2完成

--- simple-express/server.js
+++ simple-express/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connection = require('./utils/db');
+const cors = require("cors");
 
 // 利用 express 建立 express application
 let app = express();
+
+app.use(cors());
 
 app.use((req, res, next) => {
     console.log('Check 123');
@@ -21,6 +25,21 @@ app.get('/about', (req, res, next) => {
     res.send('About Us');
 });
 
-app.listen(3000, function () {
-    console.log('Listen on port 3000');
+app.get('/stock', async (req, res, next) => {
+    let results = await connection.queryAsync("SELECT * FROM stock");
+    res.json(results);
+});
+
+app.get('/stock/:stockCode', async (req, res, next) => {
+    let stockCode = req.params.stockCode;
+    let results = await connection.queryAsync("SELECT * FROM stock_price WHERE stock_id = " + stockCode);
+    res.json(results);
+});
+
+app.use((req, res) => {
+    res.status(404).json({ message: "這個頁面不存在！" });
+});
+
+app.listen(8000, function () {
+    console.log('Listen on port 8000');
 });
